28495a9bd94140349a9d628b7d18689fe42e9b68
Shivam Kumar <shivamhclme5@gmail.com>
2016-11-06 16:32:47 +0530

    Add files via upload

---
+++ shiv.java
@@ -0,0 +1,19 @@
+
+public class shiv {
+     public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		
+	
+		
+        b= a-- - --a;
+        System.out.println(b);
+		int c=a--;
+		System.out.println(c);
+		int d=a>>2;
+		System.out.println(d);
+		int e=a&b;
+		System.out.println(e);
+
+	}
+}
